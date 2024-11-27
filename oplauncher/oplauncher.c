96c2dc8b8c24d470d2403f17fada4a4fcaacb816
jgonzalez <jgonzalez@azul.com>
2024-11-27 17:42:22 +0000

    Included the proto functions and removed main from its definition

--- oplauncher/oplauncher.c
+++ oplauncher/oplauncher.c
@@ -123,28 +123,3 @@ void process_json(const char *input) {
 
     cJSON_Delete(json);
 }
-
-/**
- * MAIN EXECUTION
- */
-/*int main() {
-	char buffer[BUFFER_SIZE];
-
-	while (chrome_read_message(buffer)) {
-        // Parse the incoming JSON (a simple example without full JSON parsing)
-        char class_name[256] = "";
-        char jar_path[256] = "";
-        char params[256] = "";
-
-        sscanf(buffer, "{\"className\":\"%255[^\"]\",\"jarPath\":\"%255[^\"]\",\"params\":\"%255[^\"]\"}",
-               class_name, jar_path, params);
-
-        if (strlen(class_name) > 0 && strlen(jar_path) > 0) {
-            launch_jvm(class_name, jar_path, params);
-        } else {
-            chrome_read_message("{\"status\":\"error\",\"error\":\"Invalid message format\"}");
-        }
-    }
-    return 0;
-}*/
-
--- oplauncher/oplauncher.h
+++ oplauncher/oplauncher.h
@@ -15,5 +15,10 @@
 #include "deps/cJSON.h"
 #include "utils.h"
 
+int chrome_read_message(char *buffer);
+void chrome_send_message(const char *message);
+void launch_jvm(const char *class_name, const char *jar_path, const char *params);
+void process_json(const char *input);
+
 #endif
 
