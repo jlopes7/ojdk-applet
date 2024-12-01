433b272c8dfd90e43bc0fe3f48ea653ccea984a2
Joao Gonzalez <me@jgonzalez.tech>
2024-12-01 00:43:07 +0000

    Added more support to the Windows code, including the init of the Splash and console windows

--- oplauncher/main.c
+++ oplauncher/main.c
@@ -4,6 +4,9 @@
 #include "utils.h"
 #include "oplauncher.h"
 
+#include "ui/splash.h"
+#include "ui/java_console.h"
+
 // Global variable to store the JVM pointer
 extern jvm_launcher_t *jvm_launcher;
 
@@ -19,16 +22,58 @@ void signal_handler(int sig) {
     }
 }
 
+#if defined(_WIN32) || defined(_WIN64)
+/**
+ * Process control events from the client
+ * @param ctrlType the control type
+ * @return
+ */
+BOOL WINAPI consoleCtrlHandler(DWORD ctrlType) {
+    switch (ctrlType) {
+        case CTRL_C_EVENT: // Handle Ctrl+C
+        case CTRL_BREAK_EVENT: // Handle Ctrl+Break
+        case CTRL_CLOSE_EVENT: // Handle console close
+        case CTRL_LOGOFF_EVENT: // Handle user logoff (only in services)
+        case CTRL_SHUTDOWN_EVENT: // Handle system shutdown (only in services)
+            if ( jvm_launcher && PTR(jvm_launcher).jvm && PTR(jvm_launcher).env ) {
+                fprintf(stderr, "[FINISH OPLAUNCHER] Exit code: %ld", ctrlType);
+                // Call DestroyJavaVM to clean up the JVM
+                jvm_launcher_terminate();
+            }
+            return TRUE;
+        default:
+            return FALSE; // Pass other events to the next handler
+    }
+}
+#endif
+
 /**
  * Code Main Execution
  */
+#if defined(_WIN32) || defined(_WIN64)
+int main(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    if (!hInstance) {
+        hInstance = GetModuleHandle(NULL);
+    }
+#else
 int main(void) {
+#endif
     char buffer[BUFFER_SIZE];
     int rc = EXIT_SUCCESS;
 
 #if defined(_WIN32) || defined(_WIN64)
     char javaHome[MAX_PATH];
     GetEnvironmentVariable("OPLAUNCHER_JAVA_HOME", javaHome, MAX_PATH);
+
+    // Show the splash screen
+    show_splash_screen(hInstance);
+    // Show the Java console
+    show_console(hInstance);
+    Sleep(3000); // TODO: Just idle for a bit... (we want to show our beautiful image don't we...)
+    print_hello();
+
+    // Main application logic
+    //MessageBox(NULL, "Main application is now running!", "Oplauncher", MB_OK);
 
     // Retrieve the OPLAUNCHER_JAVA_HOME environment variable
     if ( resolveJNIDllDepsOnEnvVar("jre/bin/server") != EXIT_SUCCESS ) {
@@ -51,6 +96,12 @@ int main(void) {
         fprintf(stderr, "Error: Failed to load jvm.dll. Error code: %lu\n", GetLastError());
         return EXIT_FAILURE;
     }
+
+    // Set the custom control handler
+    if (!SetConsoleCtrlHandler(consoleCtrlHandler, TRUE)) {
+        fprintf(stderr, "Error: Could not set control handler\n");
+        return EXIT_FAILURE;
+    }
 #endif
 
     memset(buffer, 0, BUFFER_SIZE);
@@ -64,8 +115,12 @@ int main(void) {
     // Register signal handler for SIGINT and SIGTERM
     signal(SIGINT, signal_handler);  // Handle Ctrl+C
     signal(SIGTERM, signal_handler); // Handle termination signal
+#else
+    // Hide the splash screen
+    hide_splash_screen();
 #endif
 
+    fprintf(stdout, "TEST!");
     /// Trigger the dispatcher service
     while (chrome_read_message(buffer)) {
         // Parse the incoming JSON (a simple example without full JSON parsing)
