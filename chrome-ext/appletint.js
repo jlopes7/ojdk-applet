31439061e0d24874a5547320ce8b0bce79f14b7b
Joao Gonzalez <me@jgonzalez.tech>
2025-01-31 00:21:29 +0000

    Fixed the extension

--- chrome-ext/appletint.js
+++ chrome-ext/appletint.js
@@ -1,34 +1,105 @@
-const ACTION_APPLET_DETAILS_ATTRNAME = "APPLET_DETAILS_WITH_COOKIES"
+const OP_LOAD = "load_applet";
+const OPLAUNCHER_RESPONSE_CODE = "oplauncher_applet_reponse";
+const APPLET_HTML_CONTENT = `
+<html>
+  	<head>
+  	<style>
+    	body { 
+			font-family: Verdana, sans-serif; 
+			font-size: 12px; 
+			display: flex; 
+			justify-content: center; 
+			align-items: center; 
+			width: 100%; 
+			height: 100%;
+			text-align: center;
+        }
+    </style>
+    </head>
+    <body>OJDK Applet Launcher loaded</body>
+</html>
+`;
 
 /**
  * First it scans for <applet/> tags
  */
 document.addEventListener("DOMContentLoaded", () => {
+	console.info("Looking for Applet entries in the page...");
 	const applets = document.querySelectorAll("applet");
 	applets.forEach((applet) => {
 		const className = applet.getAttribute("code");
-		const archiveUrl = applet.getAttribute("archive");
+		const archiveUrl = applet.getAttribute("archive") || "";
+		const codebase = applet.getAttribute("codebase") || "";
+		const appletName = applet.getAttribute("name") || "";
+		const width = applet.getAttribute("width") || 0;
+		const height = applet.getAttribute("height") || 0;
+		// Construct the full base URL
+		const baseUrl = getBasePath(window.location.href);
 
-		console.log ("Found the following Applet", applet);
+		console.info ("Found Applet:", { className, baseUrl, archiveUrl, codebase });
+
+		// Create an iframe replacement for the applet
+		const iframe = document.createElement("iframe");
+		iframe.width = width;
+		iframe.height = height;
+		iframe.style.border = "none";
+		iframe.style.backgroundColor = "white";
+
+		// Set the iframe content dynamically
+		iframe.srcdoc = APPLET_HTML_CONTENT;
+		// Replace the applet with the iframe
+		applet.replaceWith(iframe);
+
+		// Extract applet parameters and format them as "key1=value1;key2=value2"
+		let paramArray = [];
+		applet.querySelectorAll("param").forEach((param) => {
+			const paramName = param.getAttribute("name");
+			const paramValue = param.getAttribute("value");
+			if (paramName && paramValue) {
+				paramArray.push(`${paramName}=${paramValue}`);
+			}
+		});
+		const parametersStr = paramArray.join(";");
 
 		// Fetch cookies for the current domain (this is needed to maintain the session with AppServers if necessary)
 		chrome.cookies.getAll({ url: window.location.origin }, function (cookies) {
-			// Send cookies along with applet details to the background script
+			/**
+			 * ----------------------
+			 *  OP: LOAD THE APPLET
+			 * ----------------------
+			 * --> Send cookies along with applet details to the background script
+ 			 */
 			chrome.runtime.sendMessage({
-				type: ACTION_APPLET_DETAILS_ATTRNAME,
-				className: className,
+				op: 		OP_LOAD,
+				className: 	className,
 				archiveUrl: archiveUrl,
-				cookies: cookies
+				codebase: 	codebase,
+				appletName: appletName,
+				baseUrl: 	baseUrl,
+				width: 		width,
+				height: 	height,
+				parameters: parametersStr,
+				cookies: 	cookies
 			});
 		});
+
+		// TODO: Additional operations go here!
 	});
 });
 
 // Handle responses from the background script
 chrome.runtime.onMessage.addListener((message) => {
-	if (message.action === "appletResponse") {
+	if (message.action === OPLAUNCHER_RESPONSE_CODE) {
 		console.log("Applet Response:", message.response);
 		// TODO: Implement
 	}
 });
+
+/**
+ * Returns the base URL of where the applet should be loaded from
+ */
+function getBasePath(urlString) {
+	const url = new URL(urlString);
+	return url.origin + url.pathname.substring(0, url.pathname.lastIndexOf('/') + 1);
+}
 
--- chrome-ext/background.js
+++ chrome-ext/background.js
@@ -1,72 +1,105 @@
-const ACTION_APPLET_DETAILS_ATTRNAME = "APPLET_DETAILS_WITH_COOKIES";
+const OP_LOAD = "load_applet";
+const NATIVE_SERVICE = "org.oplauncher.applet_service";
+const OPLAUNCHER_RESPONSE_CODE = "oplauncher_applet_reponse";
+const FETCH_REMOTEAPPLET = false;
 
 /**
  * Applet background service, used to communicate with the Applet external service
  */
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action === ACTION_APPLET_DETAILS_ATTRNAME) {
-        // Extract the applet details from the message
-        const { archiveUrl, codeUrl, cookies } = message.data;
+    if (message.op === OP_LOAD) {
+        /*
+         * Option 1: Loads the Applet base code and sends its bits as B64 across the wire
+         * -> May be a valid option for the future, but for now is deactivated
+         */
+        if ( FETCH_REMOTEAPPLET) {
+            console.info("Option 1 (fetch remote on Chrome Extension) was selected");
 
-        // Prepare the cookie header for fetch
-        const cookieHeader = cookies.map(cookie => `${cookie.name}=${cookie.value}`).join('; ');
+            // Extract the applet details from the message
+            const { archiveUrl, codeUrl } = message.data;
 
-        // Determine which file to download: JAR file or class file
-        let downloadUrl = null;
-        if (archiveUrl) {
-            // We have a JAR file to download
-            downloadUrl = archiveUrl;
-        } else if (codeUrl) {
-            // We have a .class file to download
-            downloadUrl = codeUrl;
-        } else {
-            console.error("No archive or code URL provided for applet.");
-			// TODO: Show the message in the browser
-            return;
+            // Determine which file to download: JAR file or class file
+            let downloadUrl = null;
+            if (archiveUrl) {
+                // We have a JAR file to download
+                downloadUrl = archiveUrl;
+            } else if (codeUrl) {
+                // We have a .class file to download
+                downloadUrl = codeUrl;
+            } else {
+                console.error("No archive or code URL provided for applet.");
+                // TODO: Show the message in the browser
+                return;
+            }
+
+            console.info("About to download the Applet codebase: ", downloadUrl)
+            // Extract the filename from the URL (either JAR or class)
+            //const urlParts = downloadUrl.split('/');
+            //const filename = urlParts[urlParts.length - 1]
+
+            // Fetch the file (either .jar or .class) with cookies to maintain session
+            fetch(message.archiveUrl || message.codebase)
+                .then(response => response.blob())
+                .then(blob => {
+                    return blob.arrayBuffer(); // Convert the Blob to ArrayBuffer
+                })
+                .then(arrayBuffer => {
+                    // Convert ArrayBuffer to Base64 (optional step to make it more message-friendly)
+                    const base64Data = arrayBufferToBase64(arrayBuffer);
+
+                    // Create the message to be sent to native host, including applet details and file content
+                    const messageToNative = {
+                        ...message.data,
+                        fileType: message.archiveUrl ? "jar" : "class",
+                        fileContent: base64Data // Pass the file content in Base64 format
+                    };
+
+                    send2OPLauncher(messageToNative, (resp, port) => {
+                        // Send response back to the content script
+                        chrome.tabs.sendMessage(sender.tab.id, {action: OPLAUNCHER_RESPONSE_CODE, response});
+                    });
+                })
+                .catch(error => {
+                    console.error("Failed to fetch or process the file:", error);
+                });
         }
+        /*
+         * Option 2: The applet sourcebase will be dealt by the OPLauncher Pilot
+         */
+        else {
+            console.info("Option 2 (Applet bits is resolved by OPLauncher) was selected");
 
-		console.log("About to download the Applet codebase: ", downloadUrl)
-        // Extract the filename from the URL (either JAR or class)
-        //const urlParts = downloadUrl.split('/');
-        //const filename = urlParts[urlParts.length - 1]
-
-        // Fetch the file (either .jar or .class) with cookies to maintain session
-        fetch(message.archiveUrl || message.codebase)
-            .then(response => response.blob())
-            .then(blob => {
-                return blob.arrayBuffer(); // Convert the Blob to ArrayBuffer
-            })
-            .then(arrayBuffer => {
-                // Convert ArrayBuffer to Base64 (optional step to make it more message-friendly)
-                const base64Data = arrayBufferToBase64(arrayBuffer);
-
-                const port = chrome.runtime.connectNative("org.oplauncher.applet_service");
-
-                // Create the message to be sent to native host, including applet details and file content
-                const messageToNative = {
-                    ...message.data,
-                    fileType: message.archiveUrl ? "jar" : "class",
-                    fileContent: base64Data // Pass the file content in Base64 format
-                };
-                
-                port.postMessage(messageToNative); // Send applet details and file content to native host
-
-                port.onMessage.addListener((response) => {
-                    console.log("Response from native host:", response);
-
-                    // Send response back to the content script
-                    chrome.tabs.sendMessage(sender.tab.id, { action: "appletResponse", response });
-                });
-
-                port.onDisconnect.addListener(() => {
-                    console.error("Native host disconnected.");
-                });
-            })
-            .catch(error => {
-                console.error("Failed to fetch or process the file:", error);
+            send2OPLauncher(message, (resp, port) => {
+                // Send response back to the content script
+                chrome.tabs.sendMessage(sender.tab.id, {action: OPLAUNCHER_RESPONSE_CODE, response});
             });
+            // TODO: Implement
+        }
     }
 });
+
+function send2OPLauncher(messageToNative, callback, callbackErr) {
+    const port = chrome.runtime.connectNative(NATIVE_SERVICE);
+
+    handlePortDisconnect = function() {
+        console.error("Native host (OPLauncher) was disconnected.");
+        callbackErr();
+    };
+
+    port.postMessage(messageToNative); // Send applet details and file content to native host
+
+    port.onMessage.addListener((response) => {
+        console.log("Response from native host:", response);
+
+        // Calls the return function (callback)
+        callback(response, port);
+    });
+
+    // Ensure onDisconnect is only added once
+    if (!port.onDisconnect.hasListener(handlePortDisconnect)) {
+        port.onDisconnect.addListener(handlePortDisconnect);
+    }
+}
 
 /**
  * Utility function to convert ArrayBuffer to Base64
